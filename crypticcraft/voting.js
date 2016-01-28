116ab1bfce126b8ad3573429596f4b6fa8449e5b
Pat Wilken <patrickwilken28@gmail.com>
2016-01-28 14:29:31 -0500

    Created voting.js

---
+++ crypticcraft/voting.js
@@ -0,0 +1,80 @@
+/* 1 */
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script><script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage1").click(function(){     $('#ChangeImage1').attr('src','http://i.imgur.com/WrqwT2i.png');    });  });
+// ]]></script>
+/* 2 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage2").click(function(){     $('#ChangeImage2').attr('src','http://i.imgur.com/DmMqFHn.png');    });  });
+// ]]></script>
+/* 3 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage3").click(function(){     $('#ChangeImage3').attr('src','http://i.imgur.com/KNZpCb8.png');    });  });
+// ]]></script>
+/* 4 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage4").click(function(){     $('#ChangeImage4').attr('src','http://i.imgur.com/HRxBpr4.png');    });  });
+// ]]></script>
+/* 5 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage5").click(function(){     $('#ChangeImage5').attr('src','http://i.imgur.com/AiI6fNp.png');    });  });
+// ]]></script>
+/* 6 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage6").click(function(){     $('#ChangeImage6').attr('src','http://i.imgur.com/qIhAOB6.png');    });  });
+// ]]></script>
+/* 7 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage7").click(function(){     $('#ChangeImage7').attr('src','http://i.imgur.com/nby111z.png');    });  });
+// ]]></script>
+/* 8 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage8").click(function(){     $('#ChangeImage8').attr('src','http://i.imgur.com/lMwOHe3.png');    });  });
+// ]]></script>
+/* 9 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage9").click(function(){     $('#ChangeImage9').attr('src','http://i.imgur.com/dN8Bg3v.png');    });  });
+// ]]></script>
+/* 10 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage10").click(function(){     $('#ChangeImage10').attr('src','http://i.imgur.com/IXYeAAP.png');    });  });
+// ]]></script>
+/* 11 */
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script><script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage11").click(function(){     $('#ChangeImage11').attr('src','http://i.imgur.com/BrCACMY.png');    });  });
+// ]]></script>
+/* 12 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage12").click(function(){     $('#ChangeImage12').attr('src','http://i.imgur.com/3DVJ4Fn.png');    });  });
+// ]]></script>
+/* 13 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage13").click(function(){     $('#ChangeImage13').attr('src','http://i.imgur.com/AthAXu9.png');    });  });
+// ]]></script>
+/* 14 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage14").click(function(){     $('#ChangeImage14').attr('src','http://i.imgur.com/XgtmWbR.png');    });  });
+// ]]></script>
+/* 15 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage15").click(function(){     $('#ChangeImage15').attr('src','http://i.imgur.com/hKSrjp7.png');    });  });
+// ]]></script>
+/* 16 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage16").click(function(){     $('#ChangeImage16').attr('src','http://i.imgur.com/ZWKsnCA.png');    });  });
+// ]]></script>
+/* 17 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage17").click(function(){     $('#ChangeImage17').attr('src','http://i.imgur.com/G0mQ9dH.png');    });  });
+// ]]></script>
+/* 18 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage18").click(function(){     $('#ChangeImage18').attr('src','http://i.imgur.com/HrvYs3K.png');    });  });
+// ]]></script>
+/* 19 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage19").click(function(){     $('#ChangeImage19').attr('src','http://i.imgur.com/pu46wVD.png');    });  });
+// ]]></script>
+/* 20 */
+<script type="text/javascript">// <![CDATA[
+$(document).ready(function(){    $("#ChangeImage20").click(function(){     $('#ChangeImage20').attr('src','http://i.imgur.com/VeOnhl9.png');    });  });
+// ]]></script>
